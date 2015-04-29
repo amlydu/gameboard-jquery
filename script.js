d31245c328b7e0238d17d76c3323d34d56794a75
Amy Dusek <amlydu@umich.edu>
2015-04-29 16:34:41 -0400

    toggling classes as well as using this to choose specific elements

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 var main = function() {
-
+	$('a.more-btn').click(function(){
+		// $('ul.more-menu').toggle(); //this would toggle ALL OF THEM
+		//console.log(this); 
+		$(this).next().toggle(); //$(this) is the event target (ONLY SELECTING THE ONE BUTTON)
+	});
+	$('li.share').click(function(){
+		$(this).next().toggle();
+	});
+	$('div.bell').click(function(){
+		$(this).toggleClass('notification active');
+	});
 };
 
 $(document).ready(main);
+
+/*****************************************************************************
+		Notes
+*****************************************************************************/
+
+/*
+Project reqs: http://s3.amazonaws.com/codecademy-content/projects/2/gameboard/README.html
+
+Objectives:
+	1. Want to click more
+	  - See more
+	2. Toggle share element
+	3. Click notification bell
+		- When clicked, toggle class
+
+	this
+		-Referring to scope
+		-Similar to self in ruby
+		-Takes the regular DOM html element and pimps it out
+
+  **Create CSS class that does the changes that you want to do and then add and remove classes as needed instead of trying to change the CSS itself
+*/
